aef378f7aecbc58f11b7043feaf4ab4bc1b3d593
shirochkin <shirochkin90@mail.ru>
2025-05-27 16:33:31 +0700

    files changed

--- Базы данных/Clients/homework_libpq_code/main.cpp
+++ Базы данных/Clients/homework_libpq_code/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include "postgre_client.h"
-#include <stdarg.h>
 #include <windows.h>
 
 int main() {
@@ -10,22 +9,16 @@ int main() {
 	SetConsoleOutputCP(1251);
 
 	try{
-	pqxx::connection c(
-		"host=localhost "
-		"port=5432 "
-		"dbname=MusicSite2 "
-		"user=postgres "
-		"password=123456");
-
+	
 		postgre_client pc;
-		//pc.create_table(tx);
-		//pc.add_client(c, 1, "Vasiliy", "Ivanov", "vasia@mail.ru");
-		//pc.add_client(c, 2, "Petr", "Petrov", "petia@mail.ru");
-		//pc.find_client_id(c, "Ivanov", "lastname");
-		//pc.add_phone(c, 1, "765765", 1);
-		//pc.alter_client(c, "Semen", "Semenov", "semka@mail.ru", 1);
-		//pc.del_phone(c, 1);
-		//pc.del_client(c, 1);
+		//pc.create_table();
+		//pc.add_client(1, "Vasiliy", "Ivanov", "vasia@mail.ru");
+		//pc.add_client(2, "Petr", "Petrov", "petia@mail.ru");
+		//pc.find_client_id("Ivanov", "lastname");
+		//pc.add_phone(1, "765765", 1);
+		//pc.alter_client("Semen", "Semenov", "semka@mail.ru", 1);
+		//pc.del_phone(1);
+		//pc.del_client(1);
 
 
 	}
--- Базы данных/Clients/homework_libpq_code/postgre_client.h
+++ Базы данных/Clients/homework_libpq_code/postgre_client.h
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <pqxx/pqxx>
 #include <string>
-#include <stdarg.h>
+#include <windows.h>
 
 
 class postgre_client {
 
+	pqxx::connection* c = new pqxx::connection(
+		"host=localhost "
+		"port=5432 "
+		"dbname=MusicSite2 "
+		"user=postgres "
+		"password=123456");
+
 public:
-	void create_table(pqxx::connection& c) {
-		pqxx::work tx{ c };
+
+	void create_table() {
+		pqxx::work tx{ *c };
 		tx.exec("create table client (id integer primary key, name varchar, lastname varchar, email varchar)");
 		tx.exec("create table phone (id integer primary key, value varchar, client_id integer)");
 		tx.commit();
 	}
 
-	void add_client(pqxx::connection& c, int id, std::string name, std::string lastname, std::string email) {
-		pqxx::work tx{ c };
+	void add_client(int id, std::string name, std::string lastname, std::string email) {
+		pqxx::work tx{ *c };
 		std::string query{ "insert into client values (" };
 		query += std::to_string(id); query += ", '"; query += name; query += "', '"; query += lastname; query += "', '"; query += email; query += "')";
 		std::cout << query << std::endl;
@@ -23,38 +31,38 @@ public:
 		tx.commit();
 	}
 
-	void add_phone(pqxx::connection& c, int id, std::string phone, int client_id) {
-		pqxx::work tx{ c };
+	void add_phone(int id, std::string phone, int client_id) {
+		pqxx::work tx{ *c };
 		std::string query{ "insert into phone values (" };
 		query += std::to_string(id), query += ", '";  query += phone; query += "', "; query += std::to_string(client_id); query += ")";
 		tx.exec(query);
 		tx.commit();
 	}
 
-	void alter_client(pqxx::connection& c, std::string new_name, std::string new_lastname, std::string new_email, int new_id) {
-		pqxx::work tx{ c };
+	void alter_client(std::string new_name, std::string new_lastname, std::string new_email, int new_id) {
+		pqxx::work tx{ *c };
 		std::string query{ "update client set name = '" };
 		query += new_name; query += "', lastname = '"; query += new_lastname; query += "', email = '"; query += new_email; query += "' where id = "; query += std::to_string(new_id);
 		tx.exec(query);
 		tx.commit();
 	}
 
-	void del_phone(pqxx::connection& c, int del_id) {
-		pqxx::work tx{ c };
+	void del_phone(int del_id) {
+		pqxx::work tx{ *c };
 		std::string query{ "delete from phone where client_id = " }; query += std::to_string(del_id);
 		tx.exec(query);
 		tx.commit();
 	}
 
-	void del_client(pqxx::connection& c, int del_id) {
-		pqxx::work tx{ c };
+	void del_client(int del_id) {
+		pqxx::work tx{ *c };
 		std::string query{ "delete from client where id = " }; query += std::to_string(del_id);
 		tx.exec(query);
 		tx.commit();
 	}
 
-	int find_client_id(pqxx::connection& c, std::string s, std::string param) {
-		pqxx::work tx{ c };
+	void find_client_id(std::string s, std::string param) {
+		pqxx::work tx{ *c };
 		std::string query{};
 		if (param == "name") {
 			query = { "select id from client where name = '" }; query += s; query += "'";
